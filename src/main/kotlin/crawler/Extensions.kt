e2e7448d5d39575844a018df1bd9d557c7b55717
mars <13093687239@163.com>
2020-08-24 22:14:19 +0800

    优化代码

--- src/main/kotlin/crawler/Extensions.kt
+++ src/main/kotlin/crawler/Extensions.kt
@@ -1,14 +1,8 @@
 package crawler
 
 import com.google.gson.Gson
-import java.util.*
-
-fun UUID.uuid(): String {
-    return this.toString().replace("-", "");
-}
 
 object Constants {
     //gson
     val gson: Gson = Gson()
-    val seriesId: String = ""
 }
--- src/main/kotlin/crawler/domain/Segment.kt
+++
@@ -1,2 +0,0 @@
-package crawler.domain
-
