9b396f16cc060b48f968ca8998b55a6eaca28c99
mars <13093687239@163.com>
2020-08-24 14:48:59 +0800

    调整代码

--- src/main/kotlin/crawler/gateway/CeairService.kt
+++ src/main/kotlin/crawler/gateway/CeairService.kt
@@ -23,44 +23,7 @@ class CeairService {
     var cookie: String = ""
 
     init {
-        //初始化拿到seriesID
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        ChromeOptions().apply {
-            addArguments(
-                "--no-sandbox",
-                "--disable-images",
-                "--user-agent = Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7"
-            )
-        }
-        val driver = ChromeDriver()
-        driver.devTools.apply {
-            createSession()
-            send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()))
-            addListener(Network.requestWillBeSent()) { entry ->
-                entry.request.url.takeIf { it.startsWith("http://observer.ceair.com/ta.png?h=event&c=button&a=submit&l=mainProcessFlightSearch") }
-                    ?.let {
-                        URLDecoder.decode(it, "UTF-8").substringAfter("p=").let {
-                            Constants.gson.fromJson(it, P::class.java)
-                        }.also {
-                            println("解析出来的seriesId = ${it.seriesid}")
-                            //保存id
-                            this@CeairService.apply {
-                                seriesId = it.seriesid
-                            }
-                        }
-                    }
-            }
-        }
-        driver["http://www.ceair.com/booking/sha-pek-200824_CNY.html"]
-        driver.manage().cookies.joinToString(separator = "; ", transform = {
-            "${it.name}=${it.value}"
-        }).also {
-            print("捕获到的cookie${it}")
-            this.cookie = it;
-        }
-        Thread.sleep(10000)
-        // 5.退出浏览器
-        driver.quit()
+        getSeriesIdAndCookie()
     }
 
     companion object {
@@ -92,15 +55,56 @@ class CeairService {
                 ),
                 Pair("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"),
                 Pair(
-                    "Cookie",
-                    this.cookie
+                    "Cookie", this.cookie
                 )
             ),
             "_=${this.seriesId}&searchCond=${searchCond.toJson()}"
-//            "_=${this.seriesId}&searchCond={\"adtCount\":1,\"chdCount\":0,\"infCount\":0,\"currency\":\"CNY\",\"tripType\":\"OW\",\"recommend\":false,\"reselect\":\"\",\"page\":\"0\",\"sortType\":\"a\",\"sortExec\":\"a\",\"seriesid\":\"${this.seriesId}\",\"segmentList\":[{\"deptCd\":\"SHA\",\"arrCd\":\"PEK\",\"deptDt\":\"2020-08-24\",\"deptAirport\":\"\",\"arrAirport\":\"\",\"deptCdTxt\":\"上海\",\"arrCdTxt\":\"北京\",\"deptCityCode\":\"SHA\",\"arrCityCode\":\"BJS\"}],\"version\":\"A.1.0\"}"
         ).exec(FlightSearchResp::class.java);
     }
 
+    private fun getSeriesIdAndCookie() {
+        //拿到seriesId 和 cookie
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        ChromeOptions().apply {
+            //设置无头
+            setHeadless(true)
+            addArguments(
+                "--no-sandbox",
+                "--disable-images",
+                "--user-agent = Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7"
+            )
+        }
+        val driver = ChromeDriver()
+        driver.devTools.apply {
+            //从链接中获取seriesId
+            createSession()
+            send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()))
+            addListener(Network.requestWillBeSent()) { entry ->
+                entry.request.url.takeIf {
+                    //判断url
+                    it.startsWith("http://observer.ceair.com/ta.png?h=event&c=button&a=submit&l=mainProcessFlightSearch")
+                }?.let {
+                    //解码
+                    URLDecoder.decode(it, "UTF-8").substringAfter("p=")
+                }?.let {
+                    //转json获取seriesId
+                    Constants.gson.fromJson(it, P::class.java).seriesid
+                }?.also {
+                    //暂存
+                    println("解析出来的seriesId = $it")
+                    this@CeairService.seriesId = it
+                }
+            }
+        }
+        driver["http://www.ceair.com/booking/sha-pek-200824_CNY.html"]
+        driver.manage().cookies.joinToString(separator = "; ", transform = { "${it.name}=${it.value}" }).also {
+            print("捕获到的cookie${it}")
+            this.cookie = it;
+        }
+        Thread.sleep(10000)
+        // 5.退出浏览器
+        driver.quit()
+    }
 
 }
 
